2d6d28709c7066224be244bce00fc4ac4a0af4d7
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-09-26 17:15:29 +0200

    First test

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const PORT = 8080;
 
-app.use(express.static(path.join(__dirname, '../frontend/crew-app/dist')));
+app.use(express.static('public'));
 
 app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Server running on http://0.0.0.0:${PORT}`);
