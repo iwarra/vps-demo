52f28ed4e2cf5892bc76ac91de6bb0ca98816079
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-09-26 17:41:09 +0200

    path fix

--- frontend/crew-app/index.js
+++ frontend/crew-app/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = 5173;
 
-app.use(express.static('../frontend/crew-app/dist'));
+app.use(express.static('dist'));
 
 app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Server running on http://0.0.0.0:${5173}`);
