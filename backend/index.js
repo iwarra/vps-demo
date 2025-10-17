5be43d320690a5278560a2d44e5728e32ba6a62b
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-10-17 22:55:02 +0200

    Cleanup / Config

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
-
-app.use(express.static('public'));
-
-app.listen(PORT, '0.0.0.0', () => {
-	console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
