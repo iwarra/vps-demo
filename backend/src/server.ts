631644b480bf5a3de8b66d58c16965c53b7b566f
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-10-21 16:15:14 +0200

    cors fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: '*',
 		methods: ['GET', 'POST', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	}),
