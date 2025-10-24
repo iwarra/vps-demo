4fd847e675a3579e5dfa10896b1147e2b0dff806
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-10-24 16:10:28 +0200

    config update

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,23 @@
-//const { createDefaultPreset } = require("ts-jest");
 import { createDefaultPreset } from 'ts-jest';
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 export default {
+	extensionsToTreatAsEsm: ['.ts'],
 	transform: {
 		'^.+\\.tsx?$': [
 			'ts-jest',
 			{
 				tsconfig: 'tsconfig.json',
+				useESM: true,
 			},
 		],
 	},
+	globals: {
+		'ts-jest': {
+			useESM: true,
+		},
+	},
+	transformIgnorePatterns: ['/node_modules/'],
+	testEnvironment: 'node',
 };
