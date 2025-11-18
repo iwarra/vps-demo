5f38eacc7a464df5f967059833ee6d437160c0c1
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-11-18 15:05:46 +0100

    Expanded routes

--- frontend/crew-app/src/router.ts
+++ frontend/crew-app/src/router.ts
@@ -4,12 +4,28 @@ import LoginPage from '../src/components/LoginPage.vue';
 import AlertSettings from '../src/components/AlertSettings.vue';
 import TableView from './components/TableView.vue';
 import DeliveryAlertSettingsTable from './components/DeliveryAlertSettingsTable.vue';
+import CategoryPage from './components/CategoryPage.vue';
 
 const routes = [
-	{ path: '/home', component: AlertSettings },
+	{
+		path: '/alert-settings/new',
+		name: 'alert-settings-new',
+		component: AlertSettings,
+	},
+	{
+		path: '/alert-settings/:id/edit',
+		name: 'alert-settings-edit',
+		component: AlertSettings,
+	},
+	{
+		path: '/alert-settings/:id/copy',
+		name: 'alert-settings-copy',
+		component: AlertSettings,
+	},
 	{ path: '/login', component: LoginPage },
 	{ path: '/table', component: TableView },
 	{ path: '/test', component: DeliveryAlertSettingsTable },
+	{ path: '/category', component: CategoryPage },
 ];
 
 export const router = createRouter({
